51c751ab6bd62fbab840c14534af9114ef6d1030
xiaoxiaopingzi <1641084175@qq.com>
2017-12-08 15:30:31 +0800

    numpy学习其三

--- numpy_1.py
+++ numpy_1.py
@@ -27,7 +27,7 @@ print(vector.shape)  # 输出行向量中元素的个数
 
 print(matrix)
 print(type(matrix))
-print(matrix.shape)  # 输出矩阵的维数，即2 x 3
+print(matrix.shape)  # 输出矩阵的行数和列数，即2 x 3
 print(matrix[:, 1])  # 用冒号占位符表示取所有元素，这里表示取矩阵第1列的所有元素
 print(matrix[:, 0:2])
 
@@ -58,3 +58,55 @@ print("对矩阵按行求和：")
 print(matrix)
 print(matrix.sum(axis=1))
 print(matrix.sum(axis=0))  # 对矩阵按列求和
+
+# 矩阵的一些属性
+# arange()函数用于生成一个array数组
+print(np.arange(15))
+a = np.arange(15).reshape(3, 5)  # reshape(3, 5)函数将行向量变换成3行5列的矩阵
+print(a)
+print(a.shape)  # shape属性用于输出矩阵的行数和列数
+print(a.ndim)  # ndim属性用于输出矩阵的维数(一般是二维的)
+print(a.dtype.name)
+print(a.size)  # 输出矩阵中元素的个数
+
+# 矩阵初始化的一些方法
+print(np.zeros((3, 4)))  # 输出一个3行4列的初始参数，注意传入的参数是(3, 4)，即元组类型的
+print(np.ones((3, 5), dtype=np.int32))  # 数据类型需要是numpy支持的类型
+print(np.arange(10, 30, 5))  # 输出从10开始，到30结束(不包括30)，间隔为5的数组
+print(np.arange(0, 2, 0.3))
+print(np.random.random((2, 3)))  # Return random floats in the half-open interval [0.0, 1.0)
+# print(help(np.random.random))
+
+print(np.linspace(0, 2 * np.pi, 10))  # 在0到2*pi之间平均找到10个数
+print(np.linspace(0, 2, 5))  # 生成5个数的数组，数组的开头和结尾分别为0.0和2.0，每两个数之间的差值为(2-0)/(5-1)
+print(np.linspace(0, 4, 10, endpoint=False))  # endpoint=False表示不包括4
+# print(help(np.linspace))
+
+
+# numpy中矩阵的加减操作
+a = np.array([20, 30, 40, 50])
+b = np.arange(4)
+print(a)
+print(b)
+print(a - b)
+print(a - 1)  # python中的广播会将数字1变为一个全1的数组
+print(b ** 2)  # 输出矩阵中每个元素的平方值
+print(a < 35)  # 判断矩阵中每个元素的值是否小于35，是就输出True，否就输出False
+
+print("----------")
+# numpy中矩阵的乘法
+A = np.array([[1, 1], [0, 1]])
+B = np.array([[2, 0], [3, 4]])
+print("矩阵A为：")
+print(A)
+print("----------")
+print("矩阵B为：")
+print(B)
+print("----------")
+print("矩阵A和B的内积(对应位置元素相乘)为：")
+print(A*B)  # A*B表示矩阵A和矩阵B的内积——对应坐标元素相乘
+print("-------------")
+print("矩阵A和B相乘的结果为：")
+print(A.dot(B))
+print("------------")
+print(np.dot(A, B))
